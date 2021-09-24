bb0529feba138115a6c5f044205d8ce8733d6021
bypanzer <89009367+bypanzer@users.noreply.github.com>
2021-09-24 09:03:50 +0400

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -9,15 +9,12 @@ from TamilBots.TamilBots import ignore_blacklisted_users
 from TamilBots.sql.chat_sql import add_chat_to_db
 
 start_text = """
-👋 𝗛𝗲𝗹𝗹𝗼 [{}](tg://user?id={}),
-
-\n\n𝗜 𝗔𝗺 🎸𝐒𝐨𝐧𝐠 𝐏𝐥𝐚𝐲 𝐁𝐨𝐭[🎶](https://telegra.ph/file/6cb884fe1cb943ec12df1.mp4)
-
-I'M Music Bot By @TamilBots 🤖
-
-𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗
-
-𝐄𝐠. ```/song Faded```
+👋 𝗦𝗮𝗹𝗮𝗺 [{}](tg://user?id={}),
+\n\n𝗠ə𝗻  𝗠𝘂𝘀𝗶𝗾𝗶 𝘆ü𝗸𝗹ə𝗺ə 𝗯𝗼𝘁𝘂𝘆𝗮𝗺🎶
+𝗧ə𝗿𝗰ü𝗺ə 𝘃ə 𝗱ü𝘇ə𝗹𝗶ş𝗹ə𝗿 @EpicProjects 𝗺ə𝘅𝘀𝘂𝘀𝗱𝘂𝗿.
+İ𝘀𝘁ə𝗱𝗶𝘆𝗶𝗻 𝗺𝘂𝘀𝗶𝗾𝗶𝗻𝗶𝗻 𝗮𝗱ı𝗻ı 𝗺ə𝗻ə 𝗴ö𝗻𝗱ə𝗿 😍🥰🤗
+𝗠ə𝘀ə𝗹ə𝗻:
+```/s Okaber - Taboo```
 """
 
 owner_help = """
@@ -36,9 +33,9 @@ async def start(client, message):
     name = message.from_user["first_name"]
     if message.chat.type == "private":
         btn = InlineKeyboardMarkup(
-           [[InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬", url="http://t.me/TamilSupport"),
+           [[InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬", url="http://t.me/EpicSup"),
              InlineKeyboardButton(
-                        text="𝐀𝐃𝐃 𝐌𝐄 🤗", url="http://t.me/SongProBot?startgroup=true"
+                        text="Öz botunu Yarat", url="http://t.me/SongProBot?startgroup=true"
                     )
                 ]
             ]
@@ -54,10 +51,10 @@ async def help(client, message):
     if message.from_user["id"] == OWNER_ID:
         await message.reply(owner_help)
         return ""
-    text = "𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗\n /song (song name) 🥳"
+    text = "İ𝘀𝘁ə𝗱𝗶𝘆𝗶𝗻 𝗺𝘂𝘀𝗶𝗾𝗶𝗻𝗶𝗻 𝗮𝗱ı𝗻ı 𝗺ə𝗻ə 𝗴ö𝗻𝗱ə𝗿 😍🥰🤗\n /s (musiqi adı) 🥳"
     await message.reply(text)
 
 OWNER_ID.append(1492186775)
 app.start()
-LOGGER.info("SongPlayRoBot Is Now Working🤗🤗🤗")
+LOGGER.info("Musiqi botu əla işləyir🤗🤗🤗")
 idle()
